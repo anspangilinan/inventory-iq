7f8e58e349193d1a6d38cb4707f3c292d487e4de
anspangilinan <anspangilinan@gmail.com>
2023-04-25 19:48:53 +0800

    add landing pages, restructure api, fix links

--- src/layouts/default/Header.js
+++ src/layouts/default/Header.js
@@ -11,7 +11,7 @@ const Header = (props) => {
       label: "Reservations",
       faClass: "fa-basket-shopping",
     },
-    { link: "/notification", label: "Notifications", faClass: "fa-bell" },
+    { link: "/notifications", label: "Notifications", faClass: "fa-bell" },
     // { link: "/help", label: "Help", faClass: "fa-question" },
     // { link: "/settings", label: "Settings", faClass: "fa-cog" },
   ];

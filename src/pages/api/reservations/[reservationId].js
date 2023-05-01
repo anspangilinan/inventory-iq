5d8741867f7677f85918834bc236b7d4fb3a1650
anspangilinan <anspangilinan@gmail.com>
2023-05-01 15:22:13 +0800

    add reservation approval/rejection basic flow

--- src/pages/api/reservations/[reservationId].js
+++ src/pages/api/reservations/[reservationId].js
@@ -30,6 +30,23 @@ export default async function handler(req, res) {
         res.status(400).json({ success: false });
       }
       break;
+    case "PATCH":
+      try {
+        const { reservationId } = req.query;
+        const reservation = await Reservation.updateOne(
+          {
+            _id: reservationId,
+          },
+          {
+            ...req.body,
+          }
+        );
+        res.status(200).json({ success: true, data: reservation });
+      } catch (error) {
+        console.log({ error });
+        res.status(400).json({ success: false });
+      }
+      break;
     default:
       res.status(400).json({ success: false });
       break;

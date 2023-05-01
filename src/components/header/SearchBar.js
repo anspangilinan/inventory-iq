e240b9c382ad5a830daf916c39cd723c5fbfa91a
anspangilinan <anspangilinan@gmail.com>
2023-05-01 09:12:48 +0800

    improve searchbar

--- src/components/header/SearchBar.js
+++ src/components/header/SearchBar.js
@@ -37,7 +37,11 @@ const SearchBar = () => {
       setShowResults(true);
     }
   }, [searchResult]);
+
   const getHighlightedText = (text, highlight) => {
+    if (highlight.length < 3) {
+      return <span>{text}</span>;
+    }
     // Split on highlight term and include term into parts, ignore case
     const parts = text.split(new RegExp(`(${highlight})`, "gi"));
     return (
@@ -58,6 +62,36 @@ const SearchBar = () => {
       </span>
     );
   };
+
+  const getSearchResults = () => {
+    if (!searchQuery || !showResults) {
+      return <></>;
+    }
+    return (
+      <div className={`${showResults && searchQuery ? "block" : "hidden"}`}>
+        <div className="absolute z-50 w-1/5">
+          <ul className="bg-white border border-gray-100 w-full ml-2">
+            {searchResult.map((equipment) => {
+              return (
+                <li className="text-sm text-blueGray-700 pr-2 pl-4 py-2 border-b-1 border-gray-100 relative cursor-pointer hover:text-orange-400 hover:bg-gray-200">
+                  <Link
+                    href={`/categories/${equipment.category.slug}/${equipment.slug}`}
+                    className=""
+                  >
+                    {getHighlightedText(equipment.name, searchQuery)}
+                    <span className="text-gray-400 ml-2">
+                      {getHighlightedText(equipment.category.name, searchQuery)}
+                    </span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <>
       <div className="w-1/2">
@@ -67,55 +101,29 @@ const SearchBar = () => {
           </label>
           <div className="relative w-full">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+              <i className="text-gray-500 fa fa-search"></i>
             </div>
             <input
               type="text"
               id="simple-search"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Search equipment or category"
+              value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
               // onBlur={() => setShowResults(false)}
               onFocus={() => setShowResults(true)}
             />
-          </div>
-        </div>
-        {searchResult && (
-          <div className={`${showResults && searchQuery ? "block" : "hidden"}`}>
-            <div className="absolute z-50 w-1/6">
-              <ul className="bg-white border border-gray-100 w-full mt-2">
-                {searchResult.map((equipment) => {
-                  return (
-                    <li className="px-2 py-1 border-b-2 border-gray-100 relative cursor-pointer hover:bg-orange-300 hover:text-gray-900">
-                      <Link
-                        href={`/categories/${equipment.category.slug}/${equipment.slug}`}
-                        className=""
-                      >
-                        {getHighlightedText(equipment.name, searchQuery)}
-                      </Link>
-                      <span className="text-gray-400">
-                        {" / "}
-                        {equipment.category.name}
-                      </span>
-                    </li>
-                  );
-                })}
-              </ul>
+            <div className="absolute inset-y-0 right-0 flex items-center">
+              <span
+                className="cursor-pointer px-5 py-1"
+                onClick={() => setSearchQuery("")}
+              >
+                <i className="text-gray-300 hover:text-orange-500 fa fa-close"></i>
+              </span>
             </div>
           </div>
-        )}
+        </div>
+        {getSearchResults()}
       </div>
     </>
   );
